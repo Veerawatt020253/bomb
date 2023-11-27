d31aa46f19cdac526ad7be01d70f4d66b1a0168f
Veerawatt020253 <53421@yupparaj.ac.th>
2023-11-27 09:45:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -19,96 +19,126 @@ for (let i = 0; i=true; i++)  {
     console.log("คุณโดนเล่นแล้ว")
     console.log("คุณโดนเล่นแล้ว")
     console.log("คุณโดนเล่นแล้ว")
+    for (let i = 0; i=true; i++)  {
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        console.log("คุณโดนเล่นแล้ว")
+        for (let i = 0; i=true; i++)  {
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            console.log("คุณโดนเล่นแล้ว")
+            for (let i = 0; i=true; i++)  {
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                console.log("คุณโดนเล่นแล้ว")
+                for (let i = 0; i=true; i++)  {
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    console.log("คุณโดนเล่นแล้ว")
+                    for (let i = 0; i=true; i++)  {
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        console.log("คุณโดนเล่นแล้ว")
+                        // alert('คุณโดนเล่นแล้ว')
+                        
+                    }
+                    // alert('คุณโดนเล่นแล้ว')
+                    
+                }
+                console.log("คุณโดนเล่นแล้ว")
+                // alert('คุณโดนเล่นแล้ว')
+                
+            }
+            // alert('คุณโดนเล่นแล้ว')
+            
+        }
+        // alert('คุณโดนเล่นแล้ว')
+        
+    }
     // alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
-    
-}
-for (let i = 0; i=true; i++)  {
-    console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
     
 }
