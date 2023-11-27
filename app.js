8ae07f998f03fcbc5f9c95a9e99147aed5243358
Veerawatt020253 <53421@yupparaj.ac.th>
2023-11-27 09:42:11 +0700

    ไฟไ

--- app.js
+++ app.js
@@ -1,33 +1,114 @@
 for (let i = 0; i=true; i++)  {
     console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
+    console.log("คุณโดนเล่นแล้ว")
     alert('คุณโดนเล่นแล้ว')
-    for (let i = 0; i=true; i++)  {
-        console.log("คุณโดนเล่นแล้ว")
-        alert('คุณโดนเล่นแล้ว')
-        for (let i = 0; i=true; i++)  {
-            console.log("คุณโดนเล่นแล้ว")
-            alert('คุณโดนเล่นแล้ว')
-            for (let i = 0; i=true; i++)  {
-                console.log("คุณโดนเล่นแล้ว")
-                alert('คุณโดนเล่นแล้ว')
-                for (let i = 0; i=true; i++)  {
-                    console.log("คุณโดนเล่นแล้ว")
-                    alert('คุณโดนเล่นแล้ว')
-                    for (let i = 0; i=true; i++)  {
-                        console.log("คุณโดนเล่นแล้ว")
-                        alert('คุณโดนเล่นแล้ว')
-                        for (let i = 0; i=true; i++)  {
-                            console.log("คุณโดนเล่นแล้ว")
-                            alert('คุณโดนเล่นแล้ว')
-                            for (let i = 0; i=true; i++)  {
-                                console.log("คุณโดนเล่นแล้ว")
-                                alert('คุณโดนเล่นแล้ว')
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-    }
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
+}
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    alert('คุณโดนเล่นแล้ว')
+    
 }
