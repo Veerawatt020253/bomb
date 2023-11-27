7fc2dba9087437eca728cf2b4ad5f31b7cca4352
Veerawatt020253 <53421@yupparaj.ac.th>
2023-11-27 09:30:35 +0700

    wa

---
+++ app.js
@@ -0,0 +1,25 @@
+for (let i = 0; i=true; i++)  {
+    console.log("คุณโดนเล่นแล้ว")
+    for (let i = 0; i=true; i++)  {
+        console.log("คุณโดนเล่นแล้ว")
+        for (let i = 0; i=true; i++)  {
+            console.log("คุณโดนเล่นแล้ว")
+            for (let i = 0; i=true; i++)  {
+                console.log("คุณโดนเล่นแล้ว")
+                for (let i = 0; i=true; i++)  {
+                    console.log("คุณโดนเล่นแล้ว")
+                    for (let i = 0; i=true; i++)  {
+                        console.log("คุณโดนเล่นแล้ว")
+                        for (let i = 0; i=true; i++)  {
+                            console.log("คุณโดนเล่นแล้ว")
+                            for (let i = 0; i=true; i++)  {
+                                console.log("คุณโดนเล่นแล้ว")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
