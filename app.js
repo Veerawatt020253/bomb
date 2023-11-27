e84240cb3ecc8f82dd0b4fa3e4115e0426e4277f
Veerawatt020253 <53421@yupparaj.ac.th>
2023-11-27 09:43:04 +0700

    ฟไก

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ for (let i = 0; i=true; i++)  {
     console.log("คุณโดนเล่นแล้ว")
     console.log("คุณโดนเล่นแล้ว")
     console.log("คุณโดนเล่นแล้ว")
-    alert('คุณโดนเล่นแล้ว')
+    // alert('คุณโดนเล่นแล้ว')
     
 }
 for (let i = 0; i=true; i++)  {
